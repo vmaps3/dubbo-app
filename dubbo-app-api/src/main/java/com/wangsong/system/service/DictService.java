1df1eba13f525bc04d42f4317d8abe4213a1df44
vmaps@qq.com <vmaps@qq.com>
2017-11-29 17:15:30 +0800

    漏掉的文件

---
+++ dubbo-app-api/src/main/java/com/wangsong/system/service/DictService.java
@@ -0,0 +1,12 @@
+package com.wangsong.system.service;
+
+
+import com.wangsong.common.service.BaseService;
+import com.wangsong.system.model.Dict;
+import com.wangsong.system.model.DictTransaction;
+
+public interface DictService extends BaseService<Dict>{
+
+	void test();
+	
+}
