d2dd678d5455653d6e034111afd76617f772a037
vmaps@qq.com <vmaps@qq.com>
2017-11-30 15:22:02 +0800

    遗漏文件

--- dubbo-app.sql
+++ dubbo-app.sql
@@ -10,13 +10,8 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- 导出 dubbo-app 的数据库结构
-CREATE DATABASE IF NOT EXISTS `dubbo-app` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `dubbo-app`;
-
-
--- 导出  表 dubbo-app.sys_dict 结构
-CREATE TABLE IF NOT EXISTS `sys_dict` (
+-- 导出  表 dubbo-app.system_dict 结构
+CREATE TABLE IF NOT EXISTS `system_dict` (
   `id` varchar(50) NOT NULL,
   `code` varchar(50) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
@@ -24,37 +19,37 @@ CREATE TABLE IF NOT EXISTS `sys_dict` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  dubbo-app.sys_dict 的数据：~2 rows (大约)
-DELETE FROM `sys_dict`;
-/*!40000 ALTER TABLE `sys_dict` DISABLE KEYS */;
-INSERT INTO `sys_dict` (`id`, `code`, `name`, `type`) VALUES
+-- 正在导出表  dubbo-app.system_dict 的数据：~2 rows (大约)
+DELETE FROM `system_dict`;
+/*!40000 ALTER TABLE `system_dict` DISABLE KEYS */;
+INSERT INTO `system_dict` (`id`, `code`, `name`, `type`) VALUES
 	('0e05c0f2-a0bc-4b75-8240-7500c9879a04', '2', '按钮', '1'),
 	('a4c4df86-d72e-4cea-b90e-753440faf6f2', '1', '菜单', '1');
-/*!40000 ALTER TABLE `sys_dict` ENABLE KEYS */;
+/*!40000 ALTER TABLE `system_dict` ENABLE KEYS */;
 
 
--- 导出  表 dubbo-app.sys_resources 结构
-CREATE TABLE IF NOT EXISTS `sys_resources` (
+-- 导出  表 dubbo-app.system_resources 结构
+CREATE TABLE IF NOT EXISTS `system_resources` (
   `id` varchar(50) NOT NULL,
   `pid` varchar(50) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
-  `url` varchar(50) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
   `type` varchar(50) DEFAULT NULL,
   `sort` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  dubbo-app.sys_resources 的数据：~22 rows (大约)
-DELETE FROM `sys_resources`;
-/*!40000 ALTER TABLE `sys_resources` DISABLE KEYS */;
-INSERT INTO `sys_resources` (`id`, `pid`, `name`, `url`, `type`, `sort`) VALUES
-	('0cf26ffd-88e9-42ca-91fb-374359e12f82', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9', '菜单管理', '/system/resources/toList.do', '1', '3'),
+-- 正在导出表  dubbo-app.system_resources 的数据：~22 rows (大约)
+DELETE FROM `system_resources`;
+/*!40000 ALTER TABLE `system_resources` DISABLE KEYS */;
+INSERT INTO `system_resources` (`id`, `pid`, `name`, `url`, `type`, `sort`) VALUES
+	('0cf26ffd-88e9-42ca-91fb-374359e12f82', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9', '菜单管理', '/system/resources/list.html', '1', '3'),
 	('0ff729fc-a43c-4a5c-a2bf-07fbeefa2520', '13fd01f1-20d8-49fd-9630-6fb7f8539920', '删除', '/system/dict/delete', '2', NULL),
-	('1', '', '菜单', '/', '1', NULL),
+	('1', '0', '菜单', '/', '1', NULL),
 	('1115d196-fd3a-4ca1-99c8-5452bae30c8f', '0cf26ffd-88e9-42ca-91fb-374359e12f82', '增加', '/system/resources/add', '2', NULL),
-	('13fd01f1-20d8-49fd-9630-6fb7f8539920', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9', '字典管理', '/system/dict/toList.do', '1', '4'),
+	('13fd01f1-20d8-49fd-9630-6fb7f8539920', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9', '字典管理', '/system/dict/list.html', '1', '4'),
 	('21e9a57d-b7a2-45b0-8359-cc21d636c0fa', '0cf26ffd-88e9-42ca-91fb-374359e12f82', '更改', '/system/resources/update', '2', NULL),
-	('2b56b958-3b0d-46d4-8988-d7c5ae1d804b', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9', '用户管理', '/system/user/toList.do', '1', '1'),
+	('2b56b958-3b0d-46d4-8988-d7c5ae1d804b', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9', '用户管理', '/system/user/list.html', '1', '1'),
 	('32680760-3ad2-4ac5-bd2f-fca4917d8f74', '2b56b958-3b0d-46d4-8988-d7c5ae1d804b', '更改', '/system/user/update', '2', NULL),
 	('51745327-1978-48ab-aa19-6986a3f628d7', 'c56d6e95-5515-492b-866a-b8555e7b647c', '添加', '/system/role/add', '2', NULL),
 	('63efc666-ee88-45f7-bdad-2b019c19eb1d', '0cf26ffd-88e9-42ca-91fb-374359e12f82', '列表', '/system/resources/list', '2', NULL),
@@ -65,99 +60,102 @@ INSERT INTO `sys_resources` (`id`, `pid`, `name`, `url`, `type`, `sort`) VALUES
 	('9ee4dade-b3af-4664-82c8-4d712f231db4', '2b56b958-3b0d-46d4-8988-d7c5ae1d804b', '删除', '/system/user/delete', '2', NULL),
 	('a9b97767-54ff-4477-a81a-274d6e2b5ce9', '1', '系统管理', '/', '1', '1'),
 	('c01620cd-4845-4a4a-a72f-1524b3c54d24', '13fd01f1-20d8-49fd-9630-6fb7f8539920', '更改', '/system/dict/update', '2', NULL),
-	('c56d6e95-5515-492b-866a-b8555e7b647c', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9', '角色管理', '/system/role/toList.do', '1', '2'),
+	('c56d6e95-5515-492b-866a-b8555e7b647c', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9', '角色管理', '/system/role/list.html', '1', '2'),
 	('c65e7c94-a3c1-43b7-a8c7-94e1d693f0b5', 'c56d6e95-5515-492b-866a-b8555e7b647c', '更改', '/system/role/update', '2', NULL),
 	('ea7a58c3-74d2-45c1-8b66-974cd82d1fbd', '13fd01f1-20d8-49fd-9630-6fb7f8539920', '列表', '/system/dict/list', '2', NULL),
 	('ecb6527f-0548-40f7-b92e-6719ff3f9556', '0cf26ffd-88e9-42ca-91fb-374359e12f82', '删除', '/system/resources/delete', '2', NULL),
 	('f0e5351e-7291-4d41-87c2-3ebfc5388d7c', 'c56d6e95-5515-492b-866a-b8555e7b647c', '列表', '/system/role/list', '2', NULL);
-/*!40000 ALTER TABLE `sys_resources` ENABLE KEYS */;
+/*!40000 ALTER TABLE `system_resources` ENABLE KEYS */;
 
 
--- 导出  表 dubbo-app.sys_role 结构
-CREATE TABLE IF NOT EXISTS `sys_role` (
+-- 导出  表 dubbo-app.system_role 结构
+CREATE TABLE IF NOT EXISTS `system_role` (
   `id` varchar(50) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  dubbo-app.sys_role 的数据：~2 rows (大约)
-DELETE FROM `sys_role`;
-/*!40000 ALTER TABLE `sys_role` DISABLE KEYS */;
-INSERT INTO `sys_role` (`id`, `name`) VALUES
-	('111', '项目经理'),
-	('f9127759-1116-4159-adcf-98dbe1d77576', '软件工程师');
-/*!40000 ALTER TABLE `sys_role` ENABLE KEYS */;
+-- 正在导出表  dubbo-app.system_role 的数据：~2 rows (大约)
+DELETE FROM `system_role`;
+/*!40000 ALTER TABLE `system_role` DISABLE KEYS */;
+INSERT INTO `system_role` (`id`, `name`) VALUES
+	('2c2cc896-21fa-4efc-a1e8-a429e0b4b70a', '1'),
+	('f9127759-1116-4159-adcf-98dbe1d77576', 'JAVA');
+/*!40000 ALTER TABLE `system_role` ENABLE KEYS */;
 
 
--- 导出  表 dubbo-app.sys_role_resources 结构
-CREATE TABLE IF NOT EXISTS `sys_role_resources` (
+-- 导出  表 dubbo-app.system_role_resources 结构
+CREATE TABLE IF NOT EXISTS `system_role_resources` (
   `id` varchar(50) NOT NULL,
   `role_id` varchar(50) DEFAULT NULL,
   `resources_id` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  dubbo-app.sys_role_resources 的数据：~22 rows (大约)
-DELETE FROM `sys_role_resources`;
-/*!40000 ALTER TABLE `sys_role_resources` DISABLE KEYS */;
-INSERT INTO `sys_role_resources` (`id`, `role_id`, `resources_id`) VALUES
-	('236563f2-3385-4c7d-b608-353ff1811568', 'f9127759-1116-4159-adcf-98dbe1d77576', 'c65e7c94-a3c1-43b7-a8c7-94e1d693f0b5'),
-	('244bc016-a930-49be-a046-280ee5083ae7', 'f9127759-1116-4159-adcf-98dbe1d77576', '63efc666-ee88-45f7-bdad-2b019c19eb1d'),
-	('31648bd0-737d-49a3-9660-2e036d3ef76d', 'f9127759-1116-4159-adcf-98dbe1d77576', 'ea7a58c3-74d2-45c1-8b66-974cd82d1fbd'),
-	('37632aa5-8104-4031-81da-b3ca142b81e9', 'f9127759-1116-4159-adcf-98dbe1d77576', 'c01620cd-4845-4a4a-a72f-1524b3c54d24'),
-	('4376daf3-cce5-4be7-9193-dfb8359b3018', 'f9127759-1116-4159-adcf-98dbe1d77576', '8d1d2815-d011-4da9-a66e-573a5f056f62'),
-	('441656ad-6d69-4a75-b34b-6187255528ea', 'f9127759-1116-4159-adcf-98dbe1d77576', '32680760-3ad2-4ac5-bd2f-fca4917d8f74'),
-	('4e5580b6-e1ea-470f-92ff-f21affd2ebf1', 'f9127759-1116-4159-adcf-98dbe1d77576', 'ecb6527f-0548-40f7-b92e-6719ff3f9556'),
-	('562814f8-9273-4bca-a5d3-79dfaecf5d01', 'f9127759-1116-4159-adcf-98dbe1d77576', '0cf26ffd-88e9-42ca-91fb-374359e12f82'),
-	('588c5174-09b4-4034-887d-d63c6d18eb3e', 'f9127759-1116-4159-adcf-98dbe1d77576', 'c56d6e95-5515-492b-866a-b8555e7b647c'),
-	('5ad6b075-64e8-44ca-8a6b-2371e85d7401', 'f9127759-1116-4159-adcf-98dbe1d77576', '1'),
-	('63210f31-23db-4c20-8dd1-5ea3d425e261', 'f9127759-1116-4159-adcf-98dbe1d77576', 'f0e5351e-7291-4d41-87c2-3ebfc5388d7c'),
-	('645e644d-a5ab-4f4f-8afd-759db09412e5', 'f9127759-1116-4159-adcf-98dbe1d77576', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9'),
-	('732c7a3c-56f6-4ae2-8927-b9c16f4b3fdb', 'f9127759-1116-4159-adcf-98dbe1d77576', '9ee4dade-b3af-4664-82c8-4d712f231db4'),
-	('90d61971-d07c-4141-9332-089fd8600f5e', 'f9127759-1116-4159-adcf-98dbe1d77576', '21e9a57d-b7a2-45b0-8359-cc21d636c0fa'),
-	('920041cb-6749-4c8e-8304-fca3fc41490f', 'f9127759-1116-4159-adcf-98dbe1d77576', '652b3c26-5085-4739-94db-ebbbb1397acf'),
-	('9ace7679-dd2b-48c3-aa1b-042776ecc478', 'f9127759-1116-4159-adcf-98dbe1d77576', '2b56b958-3b0d-46d4-8988-d7c5ae1d804b'),
-	('b9ef4d13-1632-46b3-bb1e-96600a828c74', 'f9127759-1116-4159-adcf-98dbe1d77576', '8fec88df-50c5-4a5e-800e-621d203f7a55'),
-	('bbd169ff-60f5-4cf5-a456-334a0a1dbfee', 'f9127759-1116-4159-adcf-98dbe1d77576', '1115d196-fd3a-4ca1-99c8-5452bae30c8f'),
-	('cd118549-8aac-4699-b88d-6fd5d534711c', 'f9127759-1116-4159-adcf-98dbe1d77576', '93400178-2c2f-47de-9b30-5d126b33e197'),
-	('d0a34451-03ff-439c-858f-f40b545597e3', 'f9127759-1116-4159-adcf-98dbe1d77576', '51745327-1978-48ab-aa19-6986a3f628d7'),
-	('e4c32966-232c-40f4-9f1d-a745cabb95f4', 'f9127759-1116-4159-adcf-98dbe1d77576', '0ff729fc-a43c-4a5c-a2bf-07fbeefa2520'),
-	('f41e5a1c-274d-4d2b-b5b3-7e1589e83d23', 'f9127759-1116-4159-adcf-98dbe1d77576', '13fd01f1-20d8-49fd-9630-6fb7f8539920');
-/*!40000 ALTER TABLE `sys_role_resources` ENABLE KEYS */;
+-- 正在导出表  dubbo-app.system_role_resources 的数据：~24 rows (大约)
+DELETE FROM `system_role_resources`;
+/*!40000 ALTER TABLE `system_role_resources` DISABLE KEYS */;
+INSERT INTO `system_role_resources` (`id`, `role_id`, `resources_id`) VALUES
+	('14129a14-2415-4210-89e7-128e6c6fed61', 'f9127759-1116-4159-adcf-98dbe1d77576', 'c56d6e95-5515-492b-866a-b8555e7b647c'),
+	('176fe204-c129-45fb-bf1a-306778c011a8', 'f9127759-1116-4159-adcf-98dbe1d77576', '2b56b958-3b0d-46d4-8988-d7c5ae1d804b'),
+	('262c37cd-8138-44d8-b199-a1555daf464c', 'f9127759-1116-4159-adcf-98dbe1d77576', '9ee4dade-b3af-4664-82c8-4d712f231db4'),
+	('3a718d19-83c9-4f7c-bb59-c619bf7a8422', 'f9127759-1116-4159-adcf-98dbe1d77576', '63efc666-ee88-45f7-bdad-2b019c19eb1d'),
+	('3e5eac04-54cb-4390-b2a5-8d9bd196bf71', '2c2cc896-21fa-4efc-a1e8-a429e0b4b70a', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9'),
+	('45dd1ecf-e581-4430-a20d-a20912bc186c', 'f9127759-1116-4159-adcf-98dbe1d77576', '0ff729fc-a43c-4a5c-a2bf-07fbeefa2520'),
+	('5a7e3d78-e524-4271-8ba0-60fb0d74cc40', 'f9127759-1116-4159-adcf-98dbe1d77576', '3b3b3284-eaab-48a5-a5e5-5da4e2c490af'),
+	('5d31c779-c9c4-4d49-b68f-b625778a96e3', 'f9127759-1116-4159-adcf-98dbe1d77576', 'f0e5351e-7291-4d41-87c2-3ebfc5388d7c'),
+	('5da7adcc-30c6-4c08-b89e-84549e279eac', 'f9127759-1116-4159-adcf-98dbe1d77576', '8d1d2815-d011-4da9-a66e-573a5f056f62'),
+	('6935b202-3efe-4423-b0c5-ad8594e23035', 'f9127759-1116-4159-adcf-98dbe1d77576', '0cf26ffd-88e9-42ca-91fb-374359e12f82'),
+	('6e547f35-da0c-4218-9bd4-58615c36d1ec', 'f9127759-1116-4159-adcf-98dbe1d77576', '51745327-1978-48ab-aa19-6986a3f628d7'),
+	('726bb0c5-dbff-42c3-96b0-ff841bc9e52e', '2c2cc896-21fa-4efc-a1e8-a429e0b4b70a', '2b56b958-3b0d-46d4-8988-d7c5ae1d804b'),
+	('93f621ab-32f9-4096-b78e-7f58d371dc8d', 'f9127759-1116-4159-adcf-98dbe1d77576', 'c65e7c94-a3c1-43b7-a8c7-94e1d693f0b5'),
+	('94afe788-75a9-4fb8-968d-8c97aff6c107', 'f9127759-1116-4159-adcf-98dbe1d77576', '00e70775-e85e-4e81-b24b-a4c442acf877'),
+	('9e74a77e-57e2-4ee7-a8b2-c1268fc65ec4', 'f9127759-1116-4159-adcf-98dbe1d77576', '8fec88df-50c5-4a5e-800e-621d203f7a55'),
+	('a0754a78-72c5-4eca-9a1f-0dd3484b4436', 'f9127759-1116-4159-adcf-98dbe1d77576', '1'),
+	('a65b9fc2-3569-41a6-8fd0-14172aee9b1d', 'f9127759-1116-4159-adcf-98dbe1d77576', 'ecb6527f-0548-40f7-b92e-6719ff3f9556'),
+	('a7842cad-83ba-4576-b18a-e63d52971f6b', 'f9127759-1116-4159-adcf-98dbe1d77576', '13fd01f1-20d8-49fd-9630-6fb7f8539920'),
+	('ad1c8f39-6167-4a04-add7-72344cf9f716', 'f9127759-1116-4159-adcf-98dbe1d77576', 'a9b97767-54ff-4477-a81a-274d6e2b5ce9'),
+	('b40acf45-e0bd-418a-8543-a1302922676a', 'f9127759-1116-4159-adcf-98dbe1d77576', '93400178-2c2f-47de-9b30-5d126b33e197'),
+	('cf36aa49-b4ab-4536-8e33-46b9117be9f4', 'f9127759-1116-4159-adcf-98dbe1d77576', '32680760-3ad2-4ac5-bd2f-fca4917d8f74'),
+	('d4321023-33fe-47e6-a457-941611f29ba1', 'f9127759-1116-4159-adcf-98dbe1d77576', 'ea7a58c3-74d2-45c1-8b66-974cd82d1fbd'),
+	('db8ae022-86db-4bc7-a314-8ac5e0750cda', 'f9127759-1116-4159-adcf-98dbe1d77576', '652b3c26-5085-4739-94db-ebbbb1397acf'),
+	('edbc01ff-f4a3-4eda-a851-ef0552ebbef4', 'f9127759-1116-4159-adcf-98dbe1d77576', 'c01620cd-4845-4a4a-a72f-1524b3c54d24'),
+	('f9431c22-9765-4e98-b559-9280061a4123', 'f9127759-1116-4159-adcf-98dbe1d77576', '1115d196-fd3a-4ca1-99c8-5452bae30c8f'),
+	('fc2abcc3-0565-4378-b3d2-1d6eca5e0588', 'f9127759-1116-4159-adcf-98dbe1d77576', '21e9a57d-b7a2-45b0-8359-cc21d636c0fa');
+/*!40000 ALTER TABLE `system_role_resources` ENABLE KEYS */;
 
 
--- 导出  表 dubbo-app.sys_user 结构
-CREATE TABLE IF NOT EXISTS `sys_user` (
+-- 导出  表 dubbo-app.system_user 结构
+CREATE TABLE IF NOT EXISTS `system_user` (
   `id` varchar(50) NOT NULL,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  dubbo-app.sys_user 的数据：~2 rows (大约)
-DELETE FROM `sys_user`;
-/*!40000 ALTER TABLE `sys_user` DISABLE KEYS */;
-INSERT INTO `sys_user` (`id`, `username`, `password`) VALUES
-	('18151fda-09e7-4491-a7dd-4e2bdd02ecc8', '123123', '4297f44b13955235245b2497399d7a93'),
+-- 正在导出表  dubbo-app.system_user 的数据：~1 rows (大约)
+DELETE FROM `system_user`;
+/*!40000 ALTER TABLE `system_user` DISABLE KEYS */;
+INSERT INTO `system_user` (`id`, `username`, `password`) VALUES
 	('62619962-c88c-4530-b424-ced43b30e1ea', 'wangsong', '532da1c5da9fb86bf61b2a63487f300c');
-/*!40000 ALTER TABLE `sys_user` ENABLE KEYS */;
+/*!40000 ALTER TABLE `system_user` ENABLE KEYS */;
 
 
--- 导出  表 dubbo-app.sys_user_role 结构
-CREATE TABLE IF NOT EXISTS `sys_user_role` (
+-- 导出  表 dubbo-app.system_user_role 结构
+CREATE TABLE IF NOT EXISTS `system_user_role` (
   `id` varchar(50) NOT NULL,
   `user_id` varchar(50) DEFAULT NULL,
   `role_id` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  dubbo-app.sys_user_role 的数据：~2 rows (大约)
-DELETE FROM `sys_user_role`;
-/*!40000 ALTER TABLE `sys_user_role` DISABLE KEYS */;
-INSERT INTO `sys_user_role` (`id`, `user_id`, `role_id`) VALUES
-	('5b6e768a-088a-472a-a005-7122de1f92ff', '62619962-c88c-4530-b424-ced43b30e1ea', 'f9127759-1116-4159-adcf-98dbe1d77576'),
-	('dd93d613-a3dc-4a45-aac7-7485d3caf8e2', '18151fda-09e7-4491-a7dd-4e2bdd02ecc8', '111');
-/*!40000 ALTER TABLE `sys_user_role` ENABLE KEYS */;
+-- 正在导出表  dubbo-app.system_user_role 的数据：~2 rows (大约)
+DELETE FROM `system_user_role`;
+/*!40000 ALTER TABLE `system_user_role` DISABLE KEYS */;
+INSERT INTO `system_user_role` (`id`, `user_id`, `role_id`) VALUES
+	('1f38f972-8ea6-41be-b6a1-6385f3def0f3', '62619962-c88c-4530-b424-ced43b30e1ea', 'f9127759-1116-4159-adcf-98dbe1d77576'),
+	('901733bd-a4f6-4d84-a684-2c87d6c8b5c5', '62619962-c88c-4530-b424-ced43b30e1ea', '2c2cc896-21fa-4efc-a1e8-a429e0b4b70a');
+/*!40000 ALTER TABLE `system_user_role` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
