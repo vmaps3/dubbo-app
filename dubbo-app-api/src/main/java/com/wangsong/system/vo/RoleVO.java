7f2b609416dad1264fe7b8d00e7f324d54f5b8aa
vmaps@qq.com <vmaps@qq.com>
2017-12-07 13:49:33 +0800

    bug修改

--- dubbo-app-api/src/main/java/com/wangsong/system/vo/RoleVO.java
+++ dubbo-app-api/src/main/java/com/wangsong/system/vo/RoleVO.java
@@ -6,14 +6,35 @@ import java.util.List;
 import com.wangsong.system.model.RoleResources;
 
 public class RoleVO implements Serializable{
-	List<RoleResources> roleResourcesList;
-
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<RoleResources> roleResourcesList;
+	private String id;
+    private String name;
 	public List<RoleResources> getRoleResourcesList() {
 		return roleResourcesList;
 	}
 
 	public void setRoleResourcesList(List<RoleResources> roleResourcesList) {
 		this.roleResourcesList = roleResourcesList;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
--- dubbo-app-api/src/main/java/com/wangsong/system/vo/UserVO.java
+++ dubbo-app-api/src/main/java/com/wangsong/system/vo/UserVO.java
@@ -6,8 +6,14 @@ import java.util.List;
 import com.wangsong.system.model.UserRole;
 
 public class UserVO implements Serializable{
-	String Password;
-	List<UserRole> UserRoleList;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String username;
+	private String Password;
+	private List<UserRole> UserRoleList;
 	public String getPassword() {
 		return Password;
 	}
@@ -19,6 +25,18 @@ public class UserVO implements Serializable{
 	}
 	public void setUserRoleList(List<UserRole> userRoleList) {
 		UserRoleList = userRoleList;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	
 }
