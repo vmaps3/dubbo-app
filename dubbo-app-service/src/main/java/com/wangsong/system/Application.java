8d68ff9c40c86fe9e2df7da21986ff91414596ca
vmaps@qq.com <vmaps@qq.com>
2017-12-25 11:02:01 +0800

    优化目录，公共配置

--- dubbo-app-service/src/main/java/com/wangsong/system/Application.java
+++ dubbo-app-service/src/main/java/com/wangsong/system/Application.java
@@ -3,9 +3,11 @@ package com.wangsong.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.wangsong.*.dao")
+@ComponentScan(basePackages="com.wangsong")
 public class Application {
 
 	public static void main(String[] args) {
--- dubbo-app-web/src/main/java/com/wangsong/system/ShiroLifecycleConfig.java
+++ dubbo-app-web/src/main/java/com/wangsong/system/ShiroLifecycleConfig.java
@@ -1,6 +1,5 @@
 package com.wangsong.system;
 
-import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
