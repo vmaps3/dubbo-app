845b01ec39718294620d2bc736f472045dde3faa
wangsong <vmaps@qq.com>
2018-11-07 19:42:46 +0800

    bug修改

--- dubbo-app-common/src/main/java/com/wangsong/common/model/Result.java
+++ dubbo-app-common/src/main/java/com/wangsong/common/model/Result.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Result<T> {
+public class Result<T> implements Serializable {
     private String result;
     private T data;
 }
